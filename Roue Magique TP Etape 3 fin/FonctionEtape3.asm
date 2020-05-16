5b0f466bc77e68cc7677f09c6beda3b11422c51e
Joffrey011 <dolyjoffrey@gmail.com>
2020-05-16 13:50:07 +0200

    final avec commentaires

--- Roue Magique TP Etape 3 fin/FonctionEtape3.asm
+++ Roue Magique TP Etape 3 fin/FonctionEtape3.asm
@@ -3,11 +3,6 @@
 	REQUIRE8
 	PRESERVE8
 
-;**************************************************************************
-;  Fichier Vierge.asm
-; Auteur : V.MAHOUT
-; Date :  12/11/2013
-;**************************************************************************
 
 ;***************IMPORT/EXPORT**********************************************
 	EXPORT Change_LED
@@ -34,7 +29,7 @@
 	 AREA  MesDonnees, data, readwrite
 ;**************************************************************************
 
-nbrCC DCB 0
+nbrCC DCD 0	;variable stockant le nombre de cc depuis le dernier up
 
 ;**************************************************************************
 
@@ -45,129 +40,117 @@ nbrCC DCB 0
 ;**************************************************************************
 
 
-Change_LED  PROC
+Change_LED  PROC		;procedure changeant l'etat de la LED
 		PUSH {R3,R5,R10}
 		LDR R10,=GPIOBASEB+OffsetOutput	;R10 l'adresse du output
 		MOV R5,#0x01<<10		;1 au 10 eme bit de R5
 		LDR R3,[R10]
-		EOR R3,R5
-		STRH R3,[R10]			;1 au 10 eme bit du output
-		LDR R10,=TIM1_SR
-		MOV R5,#0x01<<1
-		LDR R3,[R10]
-		BIC R3,R5
+		EOR R3,R5		;ou exlusif pour changer l'etat du 10 eme bit du output
 		STRH R3,[R10]	
 		POP {R3,R5,R10}
 		BX LR
 		ENDP
 			
 Start_Timer4  PROC		;Timer1 CC
-		PUSH {R3,R4,R5,R6,R10,LR}
-		LDR R10,=TIM1_CNT
-		LDR R4,[R10]
-		LDR R3,=Nbsecteurs
-		UDIV R4,R4,R3
-		LDR R10,=TIM4_ARR
-		STR R4,[R10]
-		LDR R10,=TIM1_CNT
+		PUSH {R3,R4,R10,LR}
+		BL Change_LED ;on change l'etat de la led
+		LDR R10,=TIM1_CNT		
+		LDR R4,[R10]	;dans R4 la valeur du compteur timer1
+		LSR R4,#PuissanceNbSecteur ;on shift a droite de n pour diviser par 2^n
 		MOV R3,#0
-		STR R3,[R10]
+		STR R3,[R10]	;on remet a 0 le compteur
+		LDR R10,=TIM4_ARR	
+		STR R4,[R10]	;on stocke la valeur du compteur divise par 2^n dans le reload du timer4
 		LDR R10,=nbrCC
-		LDRB R3,[R10]
-		ADD R3,#1	
-		STRB R3,[R10]
-		CMP R3,#3	;si c'est le 3eme cc depuis le dernier up je lance timer 4
+		LDR R3,[R10]	;dans R3 la valeur de nbrCC
+		ADD R3,#1		;on l'augmente de 1
+		STR R3,[R10]
+		CMP R3,#3	;si c'est au moins le 3eme cc depuis le dernier up je lance timer 4
 		BLT sautVit 
 		BL Run_Timer4
-sautVit	LDR R10,=TIM1_SR
-		MOV R5,#0x01<<1
+sautVit	LDR R10,=TIM1_SR	
 		LDR R3,[R10]
-		BIC R3,R5
-		STRH R3,[R10]	
-		POP {R3,R4,R5,R6,R10,LR}
+		BIC R3,#2
+		STRH R3,[R10]	;on efface le bit d'acquitement de l'interruption
+		POP {R3,R4,R10,LR}
 		BX LR
 		ENDP
 
 Arret_Timer4 PROC ;timer_up 1
-		PUSH {R3,R5,R10,LR}
-		BL Stop_Timer4
+		PUSH {R3,R10,LR}
+		LDR R10,=nbrCC
+		MOV R3,#0
+		STR R3,[R10] ;on remet a 0 nbrCC
+		BL Stop_Timer4	;on stop le timer4 lorsque l'on deborde le timer1
 		LDR R10,=TIM1_SR
-		MOV R5,#1
 		LDR R3,[R10]
-		BIC R3,R5
-		STRH R3,[R10]
-		POP {R3,R5,R10,LR}
+		BIC R3,#1
+		STRH R3,[R10] ;on efface le bit d'acquitement de l'interruption
+		POP {R3,R10,LR}
 		BX LR
 		ENDP
 
 Change_Secteur PROC	;timer 4 up
 		PUSH {R3,R4,R5,R10,LR}
-		LDR R10,=numSecteur
-		LDR R5,=mire
-		LDR R3,[R10]
-		AND R3,#7
-		MOV R4,#48
+		LDR R10,=numSecteur	
+		LDR R3,[R10]	;dans R3 le numero du secteur a afficher
+		LDR R5,=mire	;dans R5 l'addresse du debut de la mire
+		AND R3,#Nbsecteurs-1	;on met le numero du secteur modulo 2^n
+		MOV R4,#48				;l'adresse de la barette a afficer est mire+numSecteur*48
 		MLA R5,R4,R3,R5
-		PUSH {R5}
-		BL DriverPile
-		ADD SP,#4
-		ADD R3,#1
-		STR R3,[R10]
-		LDR R10,=TIM4_SR
-		MOV R5,#1
+		PUSH {R5}		;on met l'adresse en bas de la pile pour l'appel a DriverPile
+		BL DriverPile	
+		ADD SP,#4		;on deplace le debut de la pile en haut de l'argument donné a DriverPile
+		ADD R3,#1		;on augmente de 1 le numero du secteur a afficher pour la prochaine fois
+		STR R3,[R10]	
+		LDR R10,=TIM4_SR	
 		LDR R3,[R10]
-		BIC R3,R5
-		STRH R3,[R10]
+		BIC R3,#1
+		STRH R3,[R10]	;on efface le bit d'acquitement de l'interruption
 		POP {R3,R4,R5,R10,LR}
 		BX LR
 		ENDP
 
 DriverPile  PROC  ;adresse de barrette en haut de la pile
 		PUSH {R12} 
-		MOV R12,SP	;R12 l'adresse du haut de la pile(en dessous de l'argument)
-		PUSH {R0-R11,LR}
-		LDR R3,[R12,#4]	;R3 l'adresse de barrette
-		LDR R9,=DataSend
-		MOV R0,#0x01<<5
-		BL Set
-		
-		MOV R2,#0	;R2 compteur nbLed	
-b13		LDRB R4,[R3],#1	;R4 valeur courante
-		LSL R4,#24	
-		
-		MOV R6,#0	;R6 compteur nbBit
-b23		MOV R0,#0x01<<5
-		BL Reset	;reset sclk
-		AND R7,R4,#0x01<<31
-		CMP R7,#0x01<<31
-		BEQ	setSin3
-		BNE resetSin3
-setSin3	MOV R0,#0x01<<7
-		BL Set;set sin
-		B saut3
-resetSin3 MOV R0,#0x01<<7
-		BL Reset;resetSin
-saut3	LSL R4,#1
-		MOV R0,#0x01<<5
+		MOV R12,SP	;R12 l'adresse du bas de la pile(en dessous de l'argument)
+		PUSH {R0-R4,LR}
+		LDR R3,[R12,#4]	;R3 l'adresse de barrette (en R12+4 dans la pile)
+		MOV R0,#0x01<<5	;
 		BL Set	;set sclk
-		ADD R6,#1
-		CMP R6,#12
-		BNE b23
-		ADD R2,#1
-		CMP R2,#48
-		BNE b13
+		
+		MOV R1,#48	;R1 compteur nbLed	
+b1		LDRB R4,[R3],#1	;R4 valeur courante
+		LSL R4,#24	
+		MOV R2,#12	;R2 compteur nbBit
+b2		MOV R0,#0x01<<5
+		BL Reset	;reset sclk
+		LSLS R4,#1	;si le bit tout a gauche de R4 est a 1 le shift mettra le fanion c a 1
+		BCC resetSin	;en decalant on passera aussi au prochain bit pour la prochaine iteration
+		MOV R0,#0x01<<7
+		BL Set;set sin (si bit a 1)
+		B sautIF
+resetSin MOV R0,#0x01<<7
+		BL Reset;resetSin (si bit a 0)
+sautIF	MOV R0,#0x01<<5
+		BL Set	;set sclk
+		SUBS R2,#1
+		BNE b2
+		SUBS R1,#1
+		BNE b1
 		MOV R0,#0x01<<5
 		BL Reset	;reset sclk
-		MOV R10,#0
-		STRH R10,[R9]
+		LDR R12,=DataSend
+		MOV R3,#0
+		STRH R0,[R12]	;on envoie 0 a l'adresse du datasend pour indiquer que des nouvelles données ont ete transmise
 		
-		POP {R0-R11,LR}
-		POP {R12}
+		POP {R0-R4,LR}
+		POP {R12}	;les pops se font dans l'ordre inverse que les pushs
 		BX LR
 		ENDP
 
 
-Set	PROC
+Set	PROC		;procedure pour set (avec les bits a set dans R0)
 		PUSH {R10}
 		LDR R10,=GPIOBASEA+OffsetSet
 		STRH R0,[R10]
@@ -175,7 +158,7 @@ Set	PROC
 		BX LR
 		ENDP
 		
-Reset	PROC
+Reset	PROC	;procedure pour reset
 		PUSH {R10}
 		LDR R10,=GPIOBASEA+OffsetReset
 		STRH R0,[R10]
@@ -184,20 +167,5 @@ Reset	PROC
 		ENDP
 
 
-;########################################################################
-; Procédure ????
-;########################################################################
-;
-; Paramètre entrant  : ???
-; Paramètre sortant  : ???
-; Variables globales : ???
-; Registres modifiés : ???
-;------------------------------------------------------------------------
 
-
-
-
-
-
-;**************************************************************************
 	END
--- Roue Magique TP Etape 3 fin/Principale.asm
+++ Roue Magique TP Etape 3 fin/Principale.asm
@@ -23,7 +23,6 @@
 
 	IMPORT Init_Cible
 	IMPORT Run_Timer1
-	IMPORT Change_LED
 	IMPORT Start_Timer4
 	IMPORT Arret_Timer4
 	IMPORT Change_Secteur
@@ -36,7 +35,6 @@
 	AREA  mesdonnees, data, align=9, readwrite
 
 NouvTVI space 1024
-OldEtat DCB 1
 
 
 
@@ -58,25 +56,23 @@ main  	PROC
 		BL Init_Cible
 		
 		LDR R0,=NouvTVI
-        ;LDR R2,=SCB_VTOR
-		;LDR R1,[R2]
-		MOV R1,#0
-        MOV R3,#256
-bTVI 	LDR R2,[R1],#4
-        STR R2,[R0],#4
+		MOV R1,#0 ;l'adresse de l'ancienne TVI est 0
+        MOV R3,#256	;on copiera 256*4 octets
+bTVI 	LDR R2,[R1],#4 	;on lit l'ancienne TVI
+        STR R2,[R0],#4	;on recopie dans la nouvelle 
         SUBS R3,#1
         BNE bTVI
 		
 		LDR R0,=NouvTVI
 		LDR R2,=SCB_VTOR
-		STR R0,[R2]
-		LDR R0,=NouvTVI+100+64
+		STR R0,[R2]	;on change l'adresse de la TVI 
+		LDR R0,=NouvTVI+100+64	;l'interruption Timer1_CC est la 25 interruption (16*4+25*4)
 		LDR R1,=Arret_Timer4
 		STR R1,[R0]
-		ADD R0,#8
+		ADD R0,#8	;l'interruption Timer1_up est la 27 eme donc 8 octets plus loin
 		LDR R1,=Start_Timer4
 		STR R1,[R0]
-		ADD R0,#12
+		ADD R0,#12	;l'interrutpion Timer4_up est la 30 eme donc 12 octets plus loin
 		LDR R1,=Change_Secteur
 		STR R1,[R0]
 		
